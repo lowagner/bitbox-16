cab6ba680020c80eaeb45186ffbf8bd50c8a475b
lowagner <lowagner@gmail.com>
2016-12-03 20:03:34 -0600

    retain some random changes from experimental-io

--- instrument.c
+++ instrument.c
@@ -11,6 +11,13 @@
 #include <stdlib.h> // rand
 #include <string.h> // memset
 
+const uint8_t hex[64] = { 
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', // standard hex
+    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', // up to 32
+    'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', // up to 48
+    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 138, 255  // up to 64
+};
+
 const uint8_t note_name[12][2] = {
     { 'C', ' ' }, 
     { 'C', '#' }, 
